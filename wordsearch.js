2b9a1f5827be5055282b5985283d1109d637a6d8
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-20 01:34:46 +0000

    Add search word vertically and backwards

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    if (letters === undefined && word === undefined) {
+        throw new Error('Error');
+    } else if (!letters.length) {
+        return letters;
     }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    
+    for (l of horizontalJoin) {
+        let reversedl = l.split('').reverse().join('')
+        if (l.includes(word) || reversedl === word) return true
+    }
+
+    for (let i = 0; i < letters[0].length; i++) {
+        let newL = []
+        for (let j = 0; j < letters.length; j++) {
+          newL.push(letters[j][i])
+        }
+        
+        if (newL.join('') === word || newL.reverse().join('') === word ) return true
+      }
+
+    return false
 }
 
 module.exports = wordSearch
+
